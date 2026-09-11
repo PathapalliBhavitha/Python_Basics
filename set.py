2cd4c1adb08e92d6e783d983a00c5665cd0c6c57
Pathapalli Bhavitha <bhavithareddypathapally@gmail.com>
2026-09-11 23:09:45 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,29 @@
+#set ={112,434,565,67,89}
+#print(set)
+#set.add(256)
+
+#set.remove(434)
+#setprint(set)
+
+#set.pop()
+#print(set)
+
+#set.update({12,34,66,23})
+#print(set)
+
+set1 = {1,2,3,4,5,6,"weather"}
+set2 = {1,2,3,4,6} 
+#print(set1.union(set2))
+
+#print(set1.difference(set2))
+
+#print(set2.difference(set1))
+
+#print(set1.intersection(set2))
+
+#print(set1.issuperset(set2))
+
+#print(set2.issubset(set1))
+
+print(set1.issubset(set2))
+
