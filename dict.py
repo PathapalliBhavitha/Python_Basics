3a0133ebb42a7eca543a3967f8c28ec6536d49c8
Pathapalli Bhavitha <bhavithareddypathapally@gmail.com>
2026-09-11 23:11:01 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,16 @@
+dict ={
+    "Name":"bhavitha",
+    "RollNo":"248P1A0520"
+}
+#print(dict)
+
+#print(dict.get("Name"))
+
+#print(dict.items())
+
+#print(dict.keys())
+
+#print(dict.values())
+
+dict.update({"College":"ACEM"})
+print(dict)
