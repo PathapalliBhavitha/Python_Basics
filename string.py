8c9a811725c842eefc594965c83ed4a97725e107
Pathapalli Bhavitha <bhavithareddypathapally@gmail.com>
2026-09-11 23:10:42 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,20 @@
+students = "Bhavitha reddy"
+
+#indexing
+
+#print(students[3])
+
+#positive indexing ----- from 0 to end
+
+#print(students[7])
+
+#negative indexing ----from -1 to -n(reverse indexing)
+
+#print(students[-8])
+
+#slicing----this isprocess of accessing subpart of string
+#string[start:stop:step]
+
+#print(students[9:])
+
+print(students[0:8:])
