36a243bc9d0f3060d46f256de400df6405b92151
Pathapalli Bhavitha <bhavithareddypathapally@gmail.com>
2026-09-11 23:22:48 +0530

    variables

---
+++ variable.py
@@ -0,0 +1,12 @@
+name = "Bhavitha"
+age  = 20
+education = "Undergraduate"
+print(name)
+print(age)
+print(education)
+#type defining
+print(type(name))
+print(type(education))
+print(type(age))
+
+
