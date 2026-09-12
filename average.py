b1a6f3df76de4ef113ecd06f57a8e3c0fbc37571
Pathapalli Bhavitha <bhavithareddypathapally@gmail.com>
2026-09-12 06:33:29 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,6 @@
+s1 = float(input("Enter the s1 marks:"))
+s2 = float(input("Enter the s2 marks:"))
+s3 = float(input("Enter the s3 marks:"))
+sum = s1+s2+s3
+avg = sum/3
+print("The average of marks is :",avg)
