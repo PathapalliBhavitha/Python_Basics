d0bfffd92cf0b7dc53649af4c4f606198f285c43
Pathapalli Bhavitha <bhavithareddypathapally@gmail.com>
2026-09-12 09:20:51 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,8 @@
+#for i in range(10):
+   # print(i)
+#to print odd numbers
+for i in range(1,10,2):
+        print(i)
+team =["bhavitha","bavana","kundana"]
+for friend in team:
+        print(friend)
