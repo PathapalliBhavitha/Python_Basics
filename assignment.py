51994ac32b4295299ab9480d1d9e2251917cfbdf
Pathapalli Bhavitha <bhavithareddypathapally@gmail.com>
2026-09-12 07:00:13 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,19 @@
+x = float(input("Enter x value:"))
+y = float(input("Enter y value:"))
+x += y
+print(x)
+x -= y
+print(x)
+x *= y
+print(x)
+x /= y
+print(x)
+x %= y
+print(x)
+x //= y
+print(x)
+x **= y
+print(x)
+
+
+
