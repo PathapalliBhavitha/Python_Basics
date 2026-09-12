271ff67bf2273081754e7fc8a97cbe10ffe8ca75
Pathapalli Bhavitha <bhavithareddypathapally@gmail.com>
2026-09-12 08:12:28 +0530

    Add files via upload

---
+++ elif.py
@@ -0,0 +1,9 @@
+a = int(input("Enter a value:"))
+b = int(input("Enter b value:"))
+c = int(input("Enter c value:"))
+if a > b:
+    print("a is biggest value")
+elif b > c:
+    print("b is biggest value")
+else:
+    print("c is biggest value")
