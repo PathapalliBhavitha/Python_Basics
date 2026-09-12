57abe92320e6e270d6c24d10109b20fbef836920
Pathapalli Bhavitha <bhavithareddypathapally@gmail.com>
2026-09-12 08:02:47 +0530

    Add files via upload

---
+++ if.py
@@ -0,0 +1,10 @@
+tickets_theater = False
+popcorn = True
+if tickets_theater:
+    print("I will go to cinema")
+    if popcorn:
+        print("I will take popcorn")
+    else:
+        print("I will take juice")
+else:
+    print("I will go to home")
