e6e78311817a47914894071952442550b938e2c9
Pathapalli Bhavitha <bhavithareddypathapally@gmail.com>
2026-09-12 10:51:06 +0530

    Add files via upload

---
+++ var_ide.py
@@ -0,0 +1,5 @@
+def ACEM_clg_grp(students):#ACEM_clg_grp is identifier
+    students ={"swathi","geetha","vani"}#students is variable
+    for student in students:
+        print(student)
+ACEM_clg_grp(None)
